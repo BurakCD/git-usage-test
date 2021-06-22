fb9727c2c28c36bd2598e880385e21f7ed2ea807
Burak Cem Dursun <burak.c.dursun@gmail.com>
2021-06-22 20:03:59 +0300

    dosya ekleme

---
+++ app/src/main/java/com/burak/darkmode/MainActivity.java
@@ -0,0 +1,37 @@
+package com.burak.darkmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button normalMod, karanlikMod;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        normalMod = findViewById(R.id.normalMod);
+        karanlikMod = findViewById(R.id.karanlikMod);
+
+        normalMod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        });
+
+        karanlikMod.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDelegate().setLocalNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        });
+
+    }
+}
