3dac10f402ab1d25217bbb47b1d7d9929089ce34
Burak Cem Dursun <burak.c.dursun@gmail.com>
2021-06-23 18:51:25 +0300

    New scrollview sctivity

--- app/src/main/java/com/burak/darkmode/MainActivity.java
+++ app/src/main/java/com/burak/darkmode/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private String isim, Soyisim = "Burak";
